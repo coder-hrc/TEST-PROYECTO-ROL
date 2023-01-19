4e54e32dd54b09e21c21f5d35e7922d142d7a77c
Estefano Delgado <fanodev310@gmail.com>
2023-01-19 11:57:22 -0300

    testeando el primer commit, los cambios deberian estar en los archivo USUARIO.CS, INDEX.CSHTML

--- WebApplication1/Models/Usuario.cs
+++ WebApplication1/Models/Usuario.cs
@@ -1,8 +1,9 @@
-﻿namespace WebApplication1.Models
+﻿// este es un comentario de prueb para visualizar el cambio 
+namespace WebApplication1.Models
 {
     public class Usuario
     {
-        public string Nombre { get; set; }
+        public string NombreCompleto { get; set; }
         public string Correo { get; set; }
         public string Clave { get; set; }
 
